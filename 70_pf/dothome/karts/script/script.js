2cf6381dacf424356735567ace3d2ea57eb398a7
ongi <>
2025-06-11 16:20:21 +0900

    0611

--- 70_pf/dothome/karts/script/script.js
+++ 70_pf/dothome/karts/script/script.js
@@ -1,9 +1,10 @@
         $(document).ready(function(){
-            $('.main').mouseover(function(){
-                $('.sub').stop().slideDown(200);
+            $('.main>li').mouseover(function(){
+                // 메인메뉴에 마우스를 올린 서브메뉴.sub만 위에서 아래로 나타난다.
+                $(this).find('.sub').stop().slideDown(200);
             });
-            $('.main').mouseout(function(){
-                $('.sub').stop().slideUp(200);
+            $('.main>li').mouseout(function(){
+                $(this).find('.sub').stop().slideUp(200);
             });
         });
 
@@ -64,4 +65,9 @@
         $(document).ready(function(){}) > 문서가 준비되면
         mouseover(fu-)  > 마우스가 범위 안으로 들어오면
         mouseout(fu-) > 마우스가 범위 밖으로 나가면
+
+        탐색메서드
+        eq(n) n=0번부터 형제 찾기
+        siblings() 형제 찾기
+        find() 하위 요소 찾기
         */
