0b46b19247cb0acf54ce1774fdfc0f33e26a4c5a
ongi <>
2025-06-30 16:18:37 +0900

    0630

--- 70_pf/tourist2/js/nav.js
+++ 70_pf/tourist2/js/nav.js
@@ -2,11 +2,17 @@ document.ready(function () {
     $(".main>li").mouseover(function () {
         // 객체 메서드().메서드().메서드().메서드();
         // >> 객체 메서드
-        $(this).find(".sub").stop().fadeIn.css({display:'flex'});
-        $(".nav_bg").stop().animate({ height: 220 });
+        $(this).find(".sub").stop().fadeIn().css({display:'flex'});
+        $(".nav_bg").stop().animate({ height: 50 });
     });
-    $(".gnb").mouseout(function () {
-        $(".sub").stop().slideUp();
+
+    $(".main>li").mouseleave(function () {
+        $(this).find(".sub").stop().fadeIn().css({display:'none'});
+    });
+
+
+    $(".main>li").mouseout(function () {
+        $('.gnb').find(".sub").stop().fadeout().css({display:'none'});
         $(".nav_bg").stop().animate({ height: 0 });
     });
 });
