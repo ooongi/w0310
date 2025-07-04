75955d91714bbc1549c313414689ee71f2494c8f
ongi <>
2025-07-04 15:45:49 +0900

    0704

--- 70_pf/tourist2/js/slider.js
+++ 70_pf/tourist2/js/slider.js
@@ -1,16 +1,16 @@
-// JavaScript
-
 $(function () {
-  $(".bxslider").bxSlider({
-    // 'horizontal', 'vertical', 'fade'
-    mode: "fade",
-    auto: true,
-    // play/pause - 실행/일시정지
-    // autoControls: true
-    stopAutoOnClick: true,
-    // pagenation(indicator) 
-    // -> 하단 중앙의 동그라미 버튼
-    // pager: false,
-    slideWidth: 1903,
-  });
+    $(".slide").bxSlider({
+        mode: "fade", // 'horizontal(기본값)', 'vertical'
+        // 자동 슬라이드
+        auto: true,
+        // 좌우(next/prev) 버튼
+        controls: false,
+        stopAutoOnClick: true,
+        // 일지정지/실행 버튼
+        autoControls: true,
+        // 중앙의 동그라미: pagination 또는 indicator
+        // pager: false,
+        // 멀티스라이드의 하나의 슬라이드 크기
+        // slideWidth: 1440
+    });
 });
