d9cdaa95253a94522e82a58eb2fb6781440f8c51
ongi <>
2025-07-22 15:29:33 +0900

    0722

--- 70_pf/jeep/script/data.js
+++ 70_pf/jeep/script/data.js
@@ -2,22 +2,22 @@
 
 export const data = [
     {
-        img: "./images/vehicles_gche.png",
+        img: "./images/section3/vehicles_gche.png",
         txt: "GRAND CHEROKEE L",
         txt2: "A LEGACY EXTENDED",
     },
     {
-        img: "./images/vehicles_renegade.png",
+        img: "./images/section3/vehicles_renegade.png",
         txt: "RENEGADE",
         txt2: "BORN TO BE WILD",
     },
     {
-        img: "./images/vehicles_wrangler.png",
+        img: "./images/section3/vehicles_wrangler.png",
         txt: "WRANGLER",
         txt2: "FAMOUS FOR FREEDOM",
     },
     {
-        img: "./images/vehicles_avenger.png",
+        img: "./images/section3/vehicles_avenger.png",
         txt: "AVENGER",
         txt2: "ELECTRIC ADVENTURE",
     },
