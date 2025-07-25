cc3f2466609785d9cfd904ad098b4742503a95f8
ongi <>
2025-07-25 09:25:53 +0900

    0725

--- 70_pf/jeep/script/data.js
+++ 70_pf/jeep/script/data.js
@@ -1,25 +1,26 @@
 // 데이터
 
-export const data = [
+const data = [
     {
         img: "./images/section3/vehicles_gche.png",
         txt: "GRAND CHEROKEE L",
-        txt2: "A LEGACY EXTENDED",
+        txt2: "A LEGACY EXTENDED"
     },
     {
         img: "./images/section3/vehicles_renegade.png",
         txt: "RENEGADE",
-        txt2: "BORN TO BE WILD",
+        txt2: "BORN TO BE WILD"
     },
     {
         img: "./images/section3/vehicles_wrangler.png",
         txt: "WRANGLER",
-        txt2: "FAMOUS FOR FREEDOM",
+        txt2: "FAMOUS FOR FREEDOM"
     },
     {
         img: "./images/section3/vehicles_avenger.png",
         txt: "AVENGER",
-        txt2: "ELECTRIC ADVENTURE",
+        txt2: "ELECTRIC ADVENTURE"
     },
 ];
 
+export default data;
