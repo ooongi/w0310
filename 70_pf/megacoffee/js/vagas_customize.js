cbef439c85821f073fcb41f3a5755650ccdb655f
ongi <>
2025-08-22 12:34:31 +0900

    0822

---
+++ 70_pf/megacoffee/js/vagas_customize.js
@@ -0,0 +1,9 @@
+$("body").vegas({
+    slides: [
+        { src: "images/10030.jpg" },
+        { src: "images/10031.jpg" },
+        { src: "images/10032.jpg" },
+    ],
+    transition: "fade",
+    delay: 4000,
+});
